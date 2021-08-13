9efa37d50ffcb22099c6338812e89525ef035800
Arjunan323 <uarjunan97@gmail.com>
2021-08-13 09:46:48 +0530

    paypal-payment-integration

---
+++ paypal-payment-integration/src/main/java/com/arjunan/paypalpaymentintegration/PaypalPaymentIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.arjunan.paypalpaymentintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalPaymentIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalPaymentIntegrationApplication.class, args);
+	}
+
+}
---
+++ paypal-payment-integration/src/test/java/com/arjunan/paypalpaymentintegration/PaypalPaymentIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunan.paypalpaymentintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalPaymentIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
